02f5b3dcf4a48e70f6bb9b0966ee49357f30ec6d
wangtengxp <395439018@qq.com>
2023-02-14 02:39:47 +0800

    test

--- transport/main.py
+++ transport/main.py
@@ -2,7 +2,7 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
+from app import app
 if __name__=='__main__':
     app=create_app()
     app.run(host='0.0.0.0')
