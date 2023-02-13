7407d3bb2af3ed76c7d0d570ddcf27db12dcda55
wangtengxp <395439018@qq.com>
2023-02-14 02:43:46 +0800

    test

--- transport/main.py
+++ transport/main.py
@@ -2,9 +2,10 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-from app import app
+from app import create_app
+app=create_app()
 if __name__=='__main__':
-    app=create_app()
+
     app.run(host='0.0.0.0')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
