ff11cf42c1f94d777e2ba2771cff71c91891bad2
wangtengxp <395439018@qq.com>
2023-02-14 01:15:48 +0800

    main

--- main.py
+++ main.py
@@ -3,11 +3,10 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-from flask import Flask
-app=Flask(__name__)
+from transport import create_app
 
-@app.route('/')
-def hello_world():
-    return 'Hello World'
+if __name__=='__main__':
+    app=create_app()
+    app.run(host='0.0.0.0')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
