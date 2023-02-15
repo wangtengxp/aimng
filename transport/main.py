9c37491d7c37202a3b424ada5dbd84038af8edc0
wangtengxp <395439018@qq.com>
2023-02-15 08:53:56 +0800

    Update main.py

--- transport/main.py
+++ transport/main.py
@@ -2,7 +2,7 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
+from . import create_app
 app = create_app()
 if __name__=='__main__':
 
