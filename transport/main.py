db7a43570d1d03a7d9bfeeeb055d7de4c913873d
wangtengxp <395439018@qq.com>
2023-02-15 08:50:34 +0800

    Update main.py

--- transport/main.py
+++ transport/main.py
@@ -2,9 +2,8 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-from flask import Flask
 
-app = Flask(__name__)
+app = create_app()
 if __name__=='__main__':
 
     app.run(host='0.0.0.0')
